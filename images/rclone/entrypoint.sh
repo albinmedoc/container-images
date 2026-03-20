70321ba3dffc55d2cb8042e1956bfc56f5107b3d
Albin Medoc <stamline203@gmail.com>
2026-03-20 23:37:47 +0100

    Delete rclone image

--- images/rclone/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -n "$RCLONE_MOUNT_PATH" ]; then
-    fusermount3 -uz "$RCLONE_MOUNT_PATH" 2>/dev/null || \
-    umount -f "$RCLONE_MOUNT_PATH" 2>/dev/null || \
-    umount -l "$RCLONE_MOUNT_PATH" 2>/dev/null || true
-fi
-exec rclone "$@"
