b6a2de31f42fbaf5432b9cb47c3d6dff55859e7f
Albin Medoc <stamline203@gmail.com>
2026-03-20 10:23:48 +0100

    Try fix tale mount errors for rclone

--- images/rclone/entrypoint.sh
+++ images/rclone/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 if [ -n "$RCLONE_MOUNT_PATH" ]; then
-    fusermount3 -uz "$RCLONE_MOUNT_PATH" 2>/dev/null || true
+    fusermount3 -uz "$RCLONE_MOUNT_PATH" 2>/dev/null || \
+    umount -f "$RCLONE_MOUNT_PATH" 2>/dev/null || \
+    umount -l "$RCLONE_MOUNT_PATH" 2>/dev/null || true
 fi
 exec rclone "$@"
